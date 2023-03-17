13a00dea1082118f95caa397075467b890bd2f90
wise-ahmy <aabdulrahman34@outlook.com>
2023-03-17 09:00:15 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,7 @@
  * Return: Always 0 (success)
  */
  int main(void)
- {
-     printf("\"with proper grammar, but the outcome is a piece of art,");
-     return (0);
- 
-     
- }
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
