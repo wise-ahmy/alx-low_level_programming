c2a34311e564fa094a989080521ed8359c294673
wise-ahmy <aabdulrahman34@outlook.com>
2023-03-27 15:22:11 -0700

    Task 0-9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = o;
+	int x = o;
+
+	while (*(src + l) != '\0')
+	{
+		l++
+	}
+	for ( ; x < l; x++)
+	{
+		dest[x] - src[x]
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_t0_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *str);
+void puts2(char *str);
+void put_half(char sr);
+void print_array(int *a, int *b);
+char *_strcpy(char *dest, char *src);
+
+#endif
