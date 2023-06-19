2f2389ee0ec1eff576e56c54e6fab355160ce388
wise-ahmy <aabdulrahman34@outlook.com>
2023-06-19 13:46:16 -0700

    Recursion tasks

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-    int longit = 0;
-    if (*s)
-    {longit++;
+	int longit = 0;
+	
+	if (*s)
+	{longit++;
 	longit += _strlen_recursion(s + 1);
-    }
-    return (longit);
+	}
+	return (longit);
 }
